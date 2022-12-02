e28253a0d75c0dc6ee6ebfe3ea17f7743c0202c2
Victor de Francisco <vfrancisco@idealista.com>
2022-12-03 00:25:27 +0100

    Include Fuel for http stuff. First NASA api call to get astronomy picture of the day

---
+++ app/src/main/java/io/vicfran/nasalib/MainActivity.kt
@@ -0,0 +1,24 @@
+package io.vicfran.nasalib
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import io.vicfran.lib.NasaApi
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val nasaApi: NasaApi by lazy { NasaApi() }
+
+    override fun onResume() {
+        super.onResume()
+
+        nasaApi.getAstronomyPictureOfTheDay { astronomyPicture ->
+            astronomyPicture?.let {
+                Picasso.get().load(it.url).into(findViewById<ImageView>(R.id.image))
+                findViewById<TextView>(R.id.text).text = it.explanation
+            }
+        }
+    }
+
+}
---
+++ lib/src/main/java/io/vicfran/lib/NasaApi.kt
@@ -0,0 +1,36 @@
+package io.vicfran.lib
+
+import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.gson.responseObject
+
+class NasaApi {
+
+    private val TAG = "nasalib-NasaApi"
+    private val apiKey = "DEMO_KEY"
+    private val debuggable: Boolean = BuildConfig.DEBUG
+
+    init {
+        FuelManager.instance.basePath = "https://api.nasa.gov/planetary/apod?api_key=$apiKey"
+    }
+
+    fun getAstronomyPictureOfTheDay(callback: (AstronomyPicture?) -> Unit) {
+        Fuel.get("").responseObject { request, response, result ->
+            log(request, response, result)
+
+            callback(result.component1())
+        }
+    }
+
+    private fun log(request: Request, response: Response, result: Any) {
+        if (debuggable) {
+            Log.d(TAG, "Request: $request")
+            Log.d(TAG, "Response: $response")
+            Log.d(TAG, "Result: $result")
+        }
+    }
+
+}
